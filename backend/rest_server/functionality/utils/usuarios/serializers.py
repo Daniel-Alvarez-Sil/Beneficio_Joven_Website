d2311a41b78ff2b47519e676a994d4b08601472f
Daniel Alvarez Sil <A01800182@tec.mx>
2025-10-20 13:35:03 -0600

    Debugging valida-qr

--- backend/rest_server/functionality/utils/usuarios/serializers.py
+++ backend/rest_server/functionality/utils/usuarios/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import CodigoQR, Negocio, Promocion, Categoria
+from ...models import CodigoQR, Negocio, Promocion, Categoria, Usuario
 
 class NegocioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,3 +22,8 @@ class PromocionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promocion
         fields = '__all__'
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'nombre', 'apellido_paterno', 'apellido_materno', 'correo', 'telefono']
