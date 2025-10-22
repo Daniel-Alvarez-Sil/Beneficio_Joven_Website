0ebde876a0055ecbcecd39e6d05c035060e405a2
Daniel Alvarez Sil <A01800182@tec.mx>
2025-10-21 23:24:34 -0600

    Modified max limit for frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+// module.exports = {
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: '10mb',
+//     },
+//   },
+// };
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb", // string or number (bytes) is fine
+    },
+  },
 };
 
 export default nextConfig;
